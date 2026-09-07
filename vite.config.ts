73a51c9cc0cf26f2694a1d831ea8cb3c7aec8580
Koerismo <56563658+koerismo@users.noreply.github.com>
2026-09-07 16:40:48 -0400

    feat: Fix url base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/asteas-editor/',
   resolve: {
 	alias: {
 		'$lib': resolve('./src/lib')
